1728e26e1fb34825ca63b136f7ea2254568cdb76
Carlos Barros <carlos_dbs@hotmail.com>
2023-09-07 16:20:03 -0300

    refactor(Program):changes text to test application hot reload

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "I came back to review .NET Core!");
 
 app.Run();
