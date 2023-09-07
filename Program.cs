2745326423091823afd2243468ec68f24a46975b
Carlos Barros <carlos_dbs@hotmail.com>
2023-09-07 17:27:47 -0300

    adds swagger configuration

--- Program.cs
+++ Program.cs
@@ -1,6 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
+
+/* Adds swagger config */
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
+if  (app.Environment.IsDevelopment())
+{
+  app.UseDeveloperExceptionPage();
+}
+
+app.UseSwagger();
+
 app.MapGet("/", () => "I came back to review .NET Core!");
+
+app.UseSwaggerUI();
 
 app.Run();
